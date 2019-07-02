42e917e476da4f2726885316714192e9125729f5
XATTpy <zoomerrr@mail.ru>
2019-07-02 17:49:21 +0300

    get_courses_list

--- coursera.py
+++ coursera.py
@@ -1,8 +1,19 @@
 import requests
-import lxml
+from lxml import html, etree
+from random import shuffle
+
 
 def get_courses_list():
-    pass
+    courses_list = []
+    url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    request = requests.get(url).content
+    tree = html.fromstring(request)
+
+    for loc in tree.xpath('.//url/loc'):
+        url = loc.text
+        courses_list.append(url)
+    shuffle(courses_list)
+    return courses_list[:20]
 
 
 def get_course_info(course_slug):
@@ -14,4 +25,5 @@ def output_courses_info_to_xlsx(filepath):
 
 
 if __name__ == '__main__':
-    pass
+    courses_list = get_courses_list()
+    print(len(courses_list))
