42fe477a32b469a6365039c33a5d5f138fcfbcfb
XATTpy <zoomerrr@mail.ru>
2019-07-05 20:11:34 +0300

    delete selenium

--- coursera.py
+++ coursera.py
@@ -1,10 +1,8 @@
 from lxml import html, etree
 from random import shuffle
 from bs4 import BeautifulSoup
-from selenium import webdriver
 import requests
 import re
-import time
 from openpyxl import Workbook
 from os import path
 import argparse
@@ -13,18 +11,6 @@ import os
 
 def get_args():
     parser = argparse.ArgumentParser(description='Данные о курсах на Курсере.')
-    parser.add_argument(
-        'geckodriver',
-        type=str,
-        help='Введите путь к скаченному geckodriver.'
-    )
-    parser.add_argument(
-        '-b',
-        '--browser',
-        type=int,
-        default=0,
-        help='Если используете Chrome, то введите 1 (если Firefox, то ничего не вводите)'
-    )
     parser.add_argument(
         '-o',
         '--output',
@@ -38,13 +24,6 @@ def get_args():
         type=int,
         default=20,
         help='Введите количество курсов.'
-    )
-    parser.add_argument(
-        '-t',
-        '--time', 
-        type=int,
-        default=1,
-        help='Время для прогрузки страницы (в секундах)'
     )
     args = parser.parse_args()
     return args
@@ -63,29 +42,6 @@ def get_courses_list(courses_count=0):
     return courses_list[:courses_count]
 
 
-def get_response_firefox(url, driverpath):
-    driver = webdriver.Firefox(executable_path='/home/xatt/Python/devman/geckodriver')
-    driver.get(url)
-    html = driver.page_source
-    return html
-
-
-def get_response_chrome(url, driverpath):
-    driver = webdriver.Chrome(executable_path='/home/xatt/Python/devman/geckodriver')
-    driver.get(url)
-    html = driver.page_source
-    return html
-
-
-def get_start_date(soup, time4load):
-    try:
-        time.sleep(time4load)
-        start_date = soup.find(id='start-date-string').text.replace('Starts ', '')
-    except AttributeError:
-        start_date = 'Unknown'
-    return start_date
-
-
 def get_rating(soup):
     try:
         rating = soup.find('span', attrs={'itemprop': 'ratingValue'}).text
@@ -94,29 +50,24 @@ def get_rating(soup):
     return rating
 
 
-def get_weaks(soup):
+def get_weeks(soup):
     try:
-        weaks = len(soup.find(class_='Syllabus').find_all('div', text = re.compile('Week')))
+        weeks = len(soup.find(class_='Syllabus').find_all('div', text = re.compile('Week')))
     except AttributeError:
-        weaks = 'Unknown'
-    return weaks
+        weeks = 'Unknown'
+    return weeks
 
 
-def get_course_info(courses_list, browser, driverpath, time4load):
+def get_course_info(courses_list):
     courses_info = []
     for url in courses_list:
-        if not browser:
-            html = get_response_firefox(url, driverpath)
-        else:
-            html = get_response_chrome(url, driverpath)
+        html = requests.get(url).content
         soup = BeautifulSoup(html, 'html.parser')
-
         title = soup.title.text.split('|')[0]
         language = soup.select_one('.ProductGlance > :last-of-type h4').text
-        weaks = get_weaks(soup)
-        rating = get_rating(soup)
-        start_date = get_start_date(soup, time4load)
-        courses_info.append([title, language, start_date, weaks, rating])
+        start_date = soup.find(id="start-date-string").text.replace('Starts ', '')
+        weeks, rating = get_weeks(soup), get_rating(soup)
+        courses_info.append([title, language, start_date, weeks, rating])
     return courses_info
 
 
@@ -135,6 +86,6 @@ if __name__ == '__main__':
     browser = args.browser
     driverpath = args.geckodriver
     time4load = args.time
-    courses_info = get_course_info(courses_list, browser, driverpath, time4load)
+    courses_info = get_course_info(courses_list)
     filepath = path.join(args.output, 'courses.xlsx')
     output_courses_info_to_xlsx(filepath, courses_info)
